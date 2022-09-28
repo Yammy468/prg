d64790672d9a7ffc3a40eb4c1b3f735ec20d6802
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-09-28 09:17:00 +0200

    Programación Unidad1
    actividad 9

---
+++ S01Ejerccicio09.java
@@ -0,0 +1,33 @@
+public class S01Ejerccicio09 { 
+  public static void main(String[] args) {
+    
+    String s = "*";
+    String e = "^";
+    String a = "#";
+    String x = "x";
+    String r = "-";
+    String w = "w";
+    String b = "/";
+    String i = "=";
+    String u = "U";
+    String p = "·";
+    
+    System.out.println(s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s);
+    System.out.println(s+"  "+p+" "+p+"         "+s);
+    System.out.println(s+p+" "+p+p+p+" "+p+"    "+e+e+" "+s);
+    System.out.println(s+" "+p+e+" "+e+p+"        "+s);
+    System.out.println(s+" "+p+" "+u+" "+p+"   "+e+e+"   "+s);
+    System.out.println(s+p+" "+p+p+p+" "+p+"       "+s);
+    System.out.println(s+"  "+p+" "+p+"     "+x+"   "+s);
+    System.out.println(s+"         "+x+" "+x+"  "+s);
+    System.out.println(s+"        "+x+"   "+x+" "+s);
+    System.out.println(s+r+r+r+r+r+r+r+x+x+x+x+x+x+x+s);
+    System.out.println(s+"    "+w+"  "+i+i+i+i+i+i+i+s);
+    System.out.println(s+" "+w+"     "+i+"     "+i+s);
+    System.out.println(s+"    "+w+"  "+i+" "+a+a+a+" "+i+s);
+    System.out.println(s+b+b+b+b+b+b+b+i+" "+a+a+a+" "+i+s);
+    System.out.println(s+b+b+b+b+b+b+b+i+i+i+i+i+i+i+s);
+    System.out.println(s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s);
+    
+  }
+}
