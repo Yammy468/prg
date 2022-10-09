269f7fe0c9fecadde3c9d96114b7dc5f245780f9
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-09 16:41:28 +0200

    malo

---
+++ S05Ejercicio01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class S05Ejercicio02 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    for(int i = 0; i <= 100; i += 5) {
+      System.out.println(i);
+    }
+    
+   }
+  }
