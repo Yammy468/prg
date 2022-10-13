c44a816a371771fc02235f0c1c3828764887b686
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-13 10:42:33 +0200

    cambios

--- Tema_04/S04Ejercicio10.java
+++ Tema_04/S04Ejercicio10.java
@@ -4,130 +4,126 @@ public class S04Ejercicio10 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
-    System.out.print("Introduzca el dia de nacimiento: ");
-    int dia = s.nextInt();
-    System.out.print("Introduzca el mes de nacimiento: ");
-    String mes = s.next();
-           
-    System.out.println("\n");
-
+    System.out.println("Adivina cual es su Horóscopo.");
     
+    System.out.print("\nPor favor, introduzca su dia de nacimiento: ");
+    int dia = s.nextInt();
+    System.out.print("Acontinuación introduzca su mes de nacimiento: ");
+    String mes = s.next();
     String horoscopo;
     
-    switch(mes) {
-		
+    switch (mes) {
       case "enero":
-        if (dia < 21) {
-          horoscopo = "capricornio";
-        } else {
-          horoscopo = "acuario";
-        }
-      System.out.println("Su horóscopo es " + horoscopo);
-        break;
-        
-      case "febrero":
-        if (dia < 20) {
-          horoscopo = "acuario";
-        } else {
-          horoscopo = "piscis";
-        }
-      System.out.println("Su horóscopo es " + horoscopo);
-        break;
-        
+      if (dia < 21) {
+        horoscopo = "capricornio";
+      } else { 
+        horoscopo = "acuario";
+      }
+      System.out.println("\nTu horóscopo es " + horoscopo);
+      break;
+
+      case "febreo":
+      if (dia < 20) {
+        horoscopo = "acuario";
+      } else { 
+        horoscopo = "piscis";
+      }
+      System.out.println("\nTu horóscopo es " + horoscopo);
+      break;
+
       case "marzo":
-        if (dia < 21) {
-          horoscopo = "piscis";
-        } else {
-          horoscopo = "aries";
-        }
-      System.out.println("Su horóscopo es " + horoscopo);
-        break;
-        
-      case "abril":
-        if (dia < 21) {
-          horoscopo = "aries";
-        } else {
-          horoscopo = "tauro";
-        }
-      System.out.println("Su horóscopo es " + horoscopo);
-        break;
-        
-      case "mayo":
-        if (dia < 20) {
-          horoscopo = "tauro";
-        } else {
-          horoscopo = "géminis";
-        }
-      System.out.println("Su horóscopo es " + horoscopo);
-        break;
-        
-      case "junio":
-        if (dia < 22) {
-          horoscopo = "géminis";
-        } else {
-          horoscopo = "cáncer";
-        }
-      System.out.println("Su horóscopo es " + horoscopo);
-        break;
-        
-      case "julio":
-        if (dia < 22) {
-          horoscopo = "cáncer";
-        } else {
-          horoscopo = "Leo";
-        }
-      System.out.println("Su horóscopo es " + horoscopo);
-        break;
-        
-      case "agosto":
-        if (dia < 24) {
-          horoscopo = "leo";
-        } else {
-          horoscopo = "virgo";
-        }
-      System.out.println("Su horóscopo es " + horoscopo);
-        break;
-        
-      case "septiemble":
-        if (dia < 23) {
-          horoscopo = "virgo";
-        } else {
-          horoscopo = "libra";
-        }
-      System.out.println("Su horóscopo es " + horoscopo);
-        break;
-        
-      case "octuble":
-        if (dia < 23) {
-          horoscopo = "libra";
-        } else {
-          horoscopo = "escorpio";
-        }
-      System.out.println("Su horóscopo es " + horoscopo);
-        break;
-        
-      case "nobiemble":
-        if (dia < 23) {
-          horoscopo = "escorpio";
-        } else {
-          horoscopo = "sagitario";
-        }
-      System.out.println("Su horóscopo es " + horoscopo);
-        break;
-        
-      case "diciemble":
-        if (dia < 21) {
-          horoscopo = "sagitario";
-        } else {
-          horoscopo = "capricornio";
-        }
-      System.out.println("Su horóscopo es " + horoscopo);
-        break;
-      default:
-      System.out.println("No existe");
+      if (dia < 21) {
+        horoscopo = "piscis";
+      } else { 
+        horoscopo = "aries";
+      }
+      System.out.println("\nTu horóscopo es " + horoscopo);
+      break;
       
-    }
-   
+      case "abril":
+      if (dia < 21) {
+        horoscopo = "aries";
+      } else { 
+        horoscopo = "tauro";
+      }
+      System.out.println("\nTu horóscopo es " + horoscopo);
+      break;
+      
+      case "mayo":
+      if (dia < 22) {
+        horoscopo = "tauro";
+      } else { 
+        horoscopo = "géminis";
+      }
+      System.out.println("\nTu horóscopo es " + horoscopo);
+      break;
+      
+      case "junio":
+      if (dia < 23) {
+        horoscopo = "géminis";
+      } else { 
+        horoscopo = "cáncer";
+      }
+      System.out.println("\nTu horóscopo es " + horoscopo);
+      break;
+      
+      case "julio":
+      if (dia < 24) {
+        horoscopo = "cáncer";
+      } else { 
+        horoscopo = "leo";
+      }
+      System.out.println("\nTu horóscopo es " + horoscopo);
+      break;
+      
+      case "agosto":
+      if (dia < 24) {
+        horoscopo = "leo";
+      } else { 
+        horoscopo = "virgo";
+      }
+      System.out.println("\nTu horóscopo es " + horoscopo);
+      break;
+      
+      case "septiembre":
+      if (dia < 24) {
+        horoscopo = "virgo";
+      } else { 
+        horoscopo = "libra";
+      }
+      System.out.println("\nTu horóscopo es " + horoscopo);
+      break;
+      
+      case "octubre":
+      if (dia < 24) {
+        horoscopo = "libra";
+      } else { 
+        horoscopo = "escorpio";
+      }
+      System.out.println("\nTu horóscopo es " + horoscopo);
+      break;
+      
+      case "noviembre":
+      if (dia < 23) {
+        horoscopo = "escorpio";
+      } else { 
+        horoscopo = "sagitario";
+      }
+      System.out.println("\nTu horóscopo es " + horoscopo);
+      break;
+      
+      case "diciembre":
+      if (dia < 22) {
+        horoscopo = "sagitario";
+      } else { 
+        horoscopo = "capricornio";
+      }
+      System.out.println("\nTu horóscopo es " + horoscopo);
+      break;
+
+      default:
+    }   
+    
   }
   
 }
