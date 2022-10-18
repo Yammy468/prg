8adcd912487383126f9050a7f3887255a4c755b3
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-18 12:40:46 +0200

    can

--- Tema_04/S04Ejercicio15.java
+++ Tema_04/S04Ejercicio15.java
@@ -4,44 +4,55 @@ public class S04Ejercicio15 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
-    System.out.println("Pinta una pirámide.");
-    System.out.print("Introduzca un carácter de relleno: ");
-    String r = s.next();
-    System.out.println("\nElija un tipo de pirámide: ");
-    System.out.println("1. Con el vértice hacia arriba");
-    System.out.println("2. Con el vértice hacia abajo");
-    System.out.println("3. Con el vértice hacia la izquierda");
-    System.out.println("4. Con el vértice hacia la derecha" + "\n");
-   
-    int opcion = s.nextInt();
-    
-    switch(opcion) {
-      case 1:
-        System.out.println("  " + r);
-        System.out.println(" " + r + r + r);
-        System.out.println(r + r + r + r + r);
-        break;
-      case 2:
-        System.out.println(r + r + r + r + r);
-        System.out.println(" " + r + r + r);
-        System.out.println("  " + r);
-        break;
-      case 3:
-        System.out.println("    " + r);
-        System.out.println("  " + r + " " + r);
-        System.out.println(r + " " + r + " " + r);
-        System.out.println("  " + r + " " + r);
-        System.out.println("    " + r);
-        break;
-      case 4:
-        System.out.println(r);
-        System.out.println(r + " " + r);
-        System.out.println(r + " " + r + " " + r);
-        System.out.println(r + " " + r);
-        System.out.println(r);
-        break;
+    System.out.println("Dibuja un pirámide rellena.");
+    System.out.println("\nPrimero introduzca un cartacter de relleno: ");
+    String l = s.next();
+    System.out.println("\n(Opción a elegir) El vértice de la pirámide está apuntado ... ");
+    System.out.println("a) hacia arriba");
+    System.out.println("b) hacia abajo");
+    System.out.println("c) hacia la izquierda");
+    System.out.println("d) hacia la derecha");
+    System.out.print("\n===> ");
+    String opcion = s.next();
+    System.out.println("\n");
+    switch (opcion) {
+      case "a":
+      System.out.println("   " + l + "   ");
+      System.out.println("  " + l + l + l + "  ");
+      System.out.println(" " + l + l + l + l + l + " ");
+      System.out.println(l + l + l + l + l + l + l);
+      break;
+      
+      case "b":
+      System.out.println(l + l + l + l + l + l + l);
+      System.out.println(" " + l + l + l + l + l + " ");
+      System.out.println("  " + l + l + l + "  ");
+      System.out.println("   " + l + "   ");
+      break;
+      
+      case "c":
+      System.out.println("   " + l);
+      System.out.println("  " + l + l);
+      System.out.println(" " + l + l + l);
+      System.out.println(l + l + l + l);
+      System.out.println(" " + l + l + l);
+      System.out.println("  " + l + l);
+      System.out.println("   " + l);
+      break;
+
+      case "d":
+      System.out.println(l + "   ");
+      System.out.println(l + l + "  ");
+      System.out.println(l + l + l +" ");
+      System.out.println(l + l + l + l);
+      System.out.println(l + l + l +" ");
+      System.out.println(l + l + "  ");
+      System.out.println(l + "   ");
+      
       default:
-    }
+      break;
+     }
+     
   }
 }
     
