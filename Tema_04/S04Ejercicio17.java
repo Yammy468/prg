e248353eeb3ce9b8c686a6375fede5fd4f3b1a60
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-19 08:25:11 +0200

    cambio hecho

--- Tema_04/S04Ejercicio17.java
+++ Tema_04/S04Ejercicio17.java
@@ -4,9 +4,11 @@ public class S04Ejercicio17 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
-    System.out.print("Introduzca un número entero: ");
-    int n = s.nextInt();
-    System.out.println("La última cifra del número introducido es el " + (n % 10));
+    System.out.print("Por favor, introduzca un número entero: ");
+    int numero = s.nextInt();
+    
+    int ultima = numero % 10;
+    System.out.println("La última cifra es " + ultima);
     
   }
 }
