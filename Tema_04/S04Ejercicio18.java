d39c60e69a932aecdfad3bc938826b3f12bc6ea9
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-19 08:56:54 +0200

    cambio y corto

--- Tema_04/S04Ejercicio18.java
+++ Tema_04/S04Ejercicio18.java
@@ -7,25 +7,25 @@ public class S04Ejercicio18 {
     System.out.print("Por favor, introduzca un número entero (se permite hasta 5 dígitos): ");
     int numero = s.nextInt();
     
-    if (numero <10) {
+    if (numero < 10) {
       int primera = numero;
       System.out.println("La primera cifra es " + primera);
     }
     
-    if ((numero > 10) && (numero < 100)) {                                            
+    if ((numero >= 10) && (numero < 100)) {                                            
       int primera = numero / 10;
       System.out.println("La primera cifra es " + primera);
     }
     
-    if ((numero > 100) && (numero < 1000)) {                                            
+    if ((numero >= 100) && (numero < 1000)) {                                            
       int primera = numero / 100;
       System.out.println("La primera cifra es " + primera);
     }
-    if ((numero > 1000) && (numero < 10000)) {                                            
+    if ((numero >= 1000) && (numero < 10000)) {                                            
       int primera = numero / 1000;
       System.out.println("La primera cifra es " + primera);
     }
-    if ((numero > 10000) && (numero < 100000)) {                                            
+    if ((numero >= 10000) && (numero < 100000)) {                                            
       int primera = numero / 10000;
       System.out.println("La primera cifra es " + primera);
     }
