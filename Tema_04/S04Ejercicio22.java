35a7b9c1eff9aef973c74697a2f1b47bc818cca6
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-19 09:51:00 +0200

    hechos

--- Tema_04/S04Ejercicio22.java
+++ Tema_04/S04Ejercicio22.java
@@ -11,25 +11,25 @@ public class S04Ejercicio22 {
 
     switch(dia) {
       case "lunes":
-        diaNumerico = 0;
-        break;
-      case "martes":
         diaNumerico = 1;
         break;
-      case "miercoles":
+      case "martes":
         diaNumerico = 2;
         break;
-      case "jueves":
+      case "miercoles":
         diaNumerico = 3;
         break;
-      case "viernes":
+      case "jueves":
         diaNumerico = 4;
+        break;
+      case "viernes":
+        diaNumerico = 5;
         break;
       default:
         System.out.print("El día introducido no es correcto.");
     }
     
-    System.out.println("A continuación introduzca la hora (con los minutos).");
+    System.out.println("\nA continuación introduzca la hora (con los minutos).");
     
     System.out.print("Hora: ");
     int horas = s.nextInt();
@@ -39,8 +39,9 @@ public class S04Ejercicio22 {
     
     int minutosTotales = (4 * 24 * 60) + (15 * 60);
     int minutosActuales = (diaNumerico * 24 * 60) + (horas * 60) + minutos;
+    int minutosRestantes = minutosTotales - minutosActuales;
     
-    System.out.print("Faltan " + (minutosTotales - minutosActuales) + " minutos para llegar al fin de semana.");
+    System.out.print("\nFaltan " + minutosRestantes + " minutos para llegar al fin de semana.");
     
    }
   }
