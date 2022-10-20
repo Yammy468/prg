80f508a661d8b1eb12a59ba91acd2f86f4acde87
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-20 11:56:59 +0200

    ok

--- Tema_05/S05Ejercicio13.java
+++ Tema_05/S05Ejercicio13.java
@@ -4,20 +4,22 @@ public class S05Ejercicio13 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
-    System.out.println("Por favor, introduzca 10 números enteros: ");
-
-    int negativos = 0;
-    int positivos = 0;
+    System.out.println("Por favor introduzca una lista de 10 números: "); 
     
-    for (int i = 0; i < 10; i++) {
+    int positivo = 0;
+    int negativo = 0;
+    
+    int i = 0;
+    
+    while (i < 10) {
       if (s.nextInt() < 0) {
-        negativos++;
-      } else {
-        positivos++;
+        negativo ++;
+      } else { 
+        positivo ++;
       }
+      i++;
     }
-    
-    System.out.println("Ha introducido " + positivos + " positivos y " + negativos + " negativos.");
-   
+    System.out.println("Has introducido " + positivo + " números positivos y " + negativo + " negativos");
+
    }
   }
