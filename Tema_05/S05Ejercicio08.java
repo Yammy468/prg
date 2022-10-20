f416b8389803bb800a5a7ed172b2ce97f3e44ba2
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-20 09:41:50 +0200

    mi cambios

--- Tema_05/S05Ejercicio08.java
+++ Tema_05/S05Ejercicio08.java
@@ -4,11 +4,11 @@ public class S05Ejercicio08 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
-    System.out.print("Introduzca un número y le mostraré su tabla de multiplicar: ");
-    int numeroIntroducido = s.nextInt();
-        
+    System.out.print("Por favor, introduzca un número entero: ");
+    int numero = s.nextInt();
+
     for (int i = 0; i <= 10; i++) {
-      System.out.printf("%d x %2d = %3d\n", numeroIntroducido, i, numeroIntroducido * i);
+      System.out.printf(numero + "  x  " + i + "  =  " + (numero * i) + "\n");
     }
     
    }
