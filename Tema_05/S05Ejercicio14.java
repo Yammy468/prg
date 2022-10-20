8529417983ebe104a97b2bd0964aacc5c7337e7f
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-20 12:12:50 +0200

    potencia

--- Tema_05/S05Ejercicio14.java
+++ Tema_05/S05Ejercicio14.java
@@ -4,35 +4,28 @@ public class S05Ejercicio14 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
-    System.out.println("Cálculo de una potencia");
-    
-    System.out.print("Introduzca la base: ");
+    System.out.print("Introduzca una base entera: ");
     int base = s.nextInt();
-    
-    System.out.print("Introduzca el exponente: ");
+    System.out.print("acontinuación, introduzca un exponente entero: ");
     int exponente = s.nextInt();
-
+    
     int potencia = 1;
-        
+    
     if (exponente == 0) {
       potencia = 1;
     }
-    
     if (exponente > 0) {
-      for (int i = 0; i < exponente; i++) {
+      for (int i = 0; i < exponente; i++){
         potencia *= base;
       }
     }
-    
     if (exponente < 0) {
-      for (int i = 0; i < -exponente; i++) {
-        potencia *= base;
-      }
-      
-      potencia = 1/potencia;
+      for (int i = 0; i < -exponente; i++){
+       potencia *= base;
+     }
+     potencia = 1/potencia;
     }
-
-    System.out.println(base + "^" + exponente + " = " + potencia);
+    System.out.println("La potencia de " + base + "^" + exponente + " es " + potencia);
     
    }
   }
