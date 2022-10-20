4539ed4967646bf7affa9eaccc18efbfc8042a77
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-20 10:18:29 +0200

    uj

--- Tema_05/S05Ejercicio10.java
+++ Tema_05/S05Ejercicio10.java
@@ -4,22 +4,22 @@ public class S05Ejercicio10 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
+    System.out.println("Para calcular la media. ");
+    System.out.print("Introduzca una serie de números(para terminar introduzca un número negativo):\n");
+
     double numeros = 0;
     double numeroIntroducido = 0;
     double suma = 0;
-
-    System.out.println("Este programa calcula la media de los números positivos introducidos.");
-    System.out.println("Para parar, introduzca un número negativo.");
-    System.out.println("Vaya introduciendo números:");
-
+  
     while (numeroIntroducido >= 0) {
       numeroIntroducido = s.nextDouble();
-      numeros++;
+      numeros ++;
       suma += numeroIntroducido;
+      
     }
     
     System.out.print("La media de los números positivos introducidos es ");
     System.out.println((suma - numeroIntroducido) / (numeros - 1));
-   
+    
    }
   }
