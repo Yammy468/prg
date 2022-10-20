b9f68044bda7f03b584bd63a1096390c2fc70a8e
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-20 10:39:15 +0200

    caja

--- Tema_05/S05Ejercicio11.java
+++ Tema_05/S05Ejercicio11.java
@@ -4,13 +4,16 @@ public class S05Ejercicio11 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
-    System.out.print("Introduzca un número: ");
-    int numeroIntroducido = s.nextInt();
-
-    System.out.println("   n  |    n²   |    n³");
-    System.out.println("---------------------------");
-    for (int i = numeroIntroducido; i < numeroIntroducido + 5; i++) {
-      System.out.printf("%5d |%8d |%9d\n", i, i * i, i * i * i);
+    System.out.print("Introduzca un número entero: ");
+    int numero = s.nextInt();
+    int i = numero;
+    
+    System.out.println("  n  |  n²  |  n³  ");
+    System.out.println("---------------------");
+    
+    while (i < numero + 5) {
+    System.out.printf("%4d |%5d |%6d\n", i, i * i, i * i * i);
+    i ++;
     }
    
    }
