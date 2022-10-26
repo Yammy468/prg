91ef6402280d67feb05e81ed7886f242e20d7d1c
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-26 10:00:15 +0200

    bu

--- Tema_05/S05Ejercicio19.java
+++ Tema_05/S05Ejercicio19.java
@@ -4,34 +4,32 @@ public class S05Ejercicio18 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
-    int primerNumero;
-    int segundoNumero;
+    System.out.println("Dibujo de un PIRÁMIDE");
+    System.out.print("Por favor, introduzca la altura del pirámide: ");
+    int alturaIntroducida = s.nextInt();
+    System.out.print("Acontinuación, introduzca el caracter con el cual se pintara el pirámide: ");
+    String relleno = s.next();
     
-    // pide dos números y se verifica que sean distintos
-    do {
-      System.out.print("Introduzca un número entero: ");
-      primerNumero = s.nextInt();
-      System.out.print("Introduzca otro número entero distinto al anterior: ");
-      segundoNumero = s.nextInt();
-      
-      if(primerNumero == segundoNumero) {
-        System.out.print("Los números introducidos no son válidos, ");
-        System.out.println(" deben ser distintos.");
+    int altura = 1;
+    int caracter = 1;
+    int espacio = alturaIntroducida-1;
+    
+    while (altura <= alturaIntroducida) {     
+      // espacio
+      for (int i = 1; i <= espacio; i++) {
+        System.out.print(" ");
       }
-    } while (primerNumero == segundoNumero);
-    
-    // si el primer número es mayor que el segundo, se intercambian los valores
-    if (primerNumero > segundoNumero) {
-      int aux = primerNumero;
-      primerNumero = segundoNumero;
-      segundoNumero = aux;
+      // caracter
+      for (int i = 1; i <= caracter; i++) {
+        System.out.print(relleno);
+      }
+      
+     System.out.println();
+     
+     altura++;
+     espacio--;
+     caracter += 2;
     }
-    
-    for(int i = primerNumero; i <= segundoNumero; i += 7) {
-      System.out.print(i + " ");
-    }
-    
-    System.out.println();
     
    }
   }
