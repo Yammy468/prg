ca08db9f5bbfe9faee7cf5863e864800d946ff4a
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-26 08:51:19 +0200

    hecho

--- Tema_05/S05Ejercicio16.java
+++ Tema_05/S05Ejercicio16.java
@@ -4,20 +4,18 @@ public class S05Ejercicio16 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
+    boolean esPrimo = true;
     System.out.print("Introduzca un número entero y le diré si es primo: ");
     int numeroIntroducido = s.nextInt();
-
-    boolean esPrimo = true;
     
     for (int i = 2; i < numeroIntroducido; i++) {
       if ((numeroIntroducido % i) == 0) {
         esPrimo = false;
       }
     }
-        
     if (esPrimo) {
       System.out.println("El número introducido es primo.");
-    } else {
+    } else { 
       System.out.println("El número introducido no es primo.");
     }
     
