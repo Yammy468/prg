35f9cd8cf2ae96590f9cae9347203280ed786a1a
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-26 10:22:35 +0200

    4

--- Tema_05/S05Ejercicio20.java
+++ Tema_05/S05Ejercicio20.java
@@ -4,27 +4,26 @@ public class S05Ejercicio20 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
-    System.out.print("Por favor, introduzca la altura de la pirámide: ");
+    System.out.println("Dibujo de un PIRÁMIDE");
+    System.out.print("Por favor, introduzca la altura del pirámide: ");
     int alturaIntroducida = s.nextInt();
-
-    System.out.print("Introduzca el carácter de relleno: ");
+    System.out.print("Acontinuación, introduzca el caracter con el cual se pintara el pirámide: ");
     String relleno = s.next();
     
     int altura = 1;
+    int espacios = alturaIntroducida-1;
+    int hueco = 0;
     int i = 0;
-    int espaciosPorDelante = alturaIntroducida - 1;
-    int espaciosInternos = 0;
     
-    while (altura < alturaIntroducida) {
-      
-      // inserta espacios delante
-      for (i = 1; i <= espaciosPorDelante; i++) {
+    while (altura < alturaIntroducida) {     
+      // espacios 
+      for (i = 1; i <= espacios; i++) {
         System.out.print(" ");
       }
       
       // pinta la línea
       System.out.print(relleno);
-      for (i = 1; i < espaciosInternos; i++) {
+      for (i = 1; i < hueco; i++) {
         System.out.print(" ");
       }
       
@@ -34,11 +33,10 @@ public class S05Ejercicio20 {
       
       System.out.println();
       altura++;
-      espaciosPorDelante--;
-      espaciosInternos += 2;
-    } // while ////////////////////////////
-    
-    // base de la pirámide
+      espacios--;
+      hueco += 2;
+    } 
+    // base
     for (i = 1; i < altura*2; i++) {
       System.out.print(relleno);
     }
