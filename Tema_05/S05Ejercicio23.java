86e3818baef9738e9a46e361e4890f8402f8b289
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-10-27 09:16:00 +0200

    e23

--- Tema_05/S05Ejercicio23.java
+++ Tema_05/S05Ejercicio23.java
@@ -20,6 +20,22 @@ public class S05Ejercicio23 {
     System.out.println("Ha introducido un total de " + numeroDeElementos + " números.");
     System.out.println("La suma total es " + suma + ".");
     System.out.println("La media es " + suma / (double) numeroDeElementos);
+    System.out.println("Por favor, introduzca los números.");
+    System.out.println("El programa terminará cuando la suma de los números sea mayor que 10000.");
+    
+    int numeroIntroducido;
+    int suma = 0;
+    int numeroTotal = 0;
+    
+    do {
+      numeroIntroducido = s.nextInt();
+      suma += numeroIntroducido;
+      numeroTotal++;
+    } while (suma <= 10000);
+    
+    System.out.println("Ha introducido un total de " + numeroTotal + " números.");
+    System.out.println("La suma total es " + suma + ".");
+    System.out.println("La media es " + suma / (double) numeroTotal);
     
    }
   }
