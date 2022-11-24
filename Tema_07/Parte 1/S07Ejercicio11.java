532882b39705d0c43f7b698c7c6a9bde5c05ab79
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-11-24 11:35:06 +0100

    cambio nuevo

--- Tema_07/Parte 1/S07Ejercicio11.java
+++ Tema_07/Parte 1/S07Ejercicio11.java
@@ -7,82 +7,71 @@ public class S07Ejercicio11 {
     int[] n = new int[10];
     int[] primo = new int[10];
     int[] noPrimo = new int[10];
-    int i;
-    int j;
+    int i, j;
     int primos = 0;
     int noPrimos = 0;
-    boolean esPrimo = false;
+    boolean esPrimo = true;
     
-    System.out.println("Introduzca 10 números separados por INTRO:");
-
-    for (i = 0; i < 10; i++) {
+    System.out.println("Inrtroduzca 10 números:");
+    
+    for(i = 0; i < 10; i++) {
       n[i] = s.nextInt();
-      
-      // Comprueba si el número es o no primo.
       esPrimo = true;
       for (j = 2; j < n[i] - 1; j++) {
         if (n[i] % j == 0) {
           esPrimo = false;
         }
       }
-      
-      // Si el número es primo, se mete en un array y si
-      // no es primo, se mete en otro diferente.
       if (esPrimo) {
         primo[primos++] = n[i];
       } else {
         noPrimo[noPrimos++] = n[i];
       }
-    }
+     }
     
-    System.out.println("\n\nArray original:\n");
-
+    System.out.println("\nArray INICIAL\n");
+    
     System.out.print(" Índice ");
-    for (i = 0; i < 10; i++) {
-      System.out.printf("│%4d ", i);
+    for(i = 0; i < 10; i++) {
+      System.out.printf("|%4d ", i);
     }
     System.out.println("|");
-    
-    for(i = 0; i< 75; i++) {
+    for(i = 0; i < 75; i++) {
       System.out.print("-");
     }
     System.out.println("-");
-    
     System.out.print(" Valor  ");
-    for (i = 0; i < 10; i++) {
-      System.out.printf("│%4d ", n[i]);
+    for(i = 0; i < 10; i++) {
+      System.out.printf("|%4d ", n[i]);
     }
-    System.out.println("│");
+    System.out.println("|");
     
     // Lo primo
-    for (i = 0; i < primos; i++) {
+    for(i = 0; i < primos; i++) {
       n[i] = primo[i];
     }
-    
     // Lo no primo
-    for (i = primos; i < primos + noPrimos; i++) {
+    for(i = primos; i < 10; i++) {
       n[i] = noPrimo[i - primos];
     }
-
-    // Muestra el resultado.
-    System.out.println("\n\nArray con los primos al principio:\n");
+    
+    // Resultado
+    System.out.println("\n\nArray RESULTADO\n");
     
     System.out.print(" Índice ");
-    for (i = 0; i < 10; i++) {
-      System.out.printf("│%4d ", i);
+    for(i = 0; i < 10; i++) {
+      System.out.printf("|%4d ", i);
     }
     System.out.println("|");
-    
-    for(i = 0; i< 75; i++) {
+    for(i = 0; i < 75; i++) {
       System.out.print("-");
     }
     System.out.println("-");
-    
     System.out.print(" Valor  ");
-    for (i = 0; i < 10; i++) {
-      System.out.printf("│%4d ", n[i]);
+    for(i = 0; i < 10; i++) {
+      System.out.printf("|%4d ", n[i]);
     }
-    System.out.println("│");
+    System.out.println("|");
     
   }
 }
