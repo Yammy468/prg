25f029f89e125b7ead281d50d5a49f22dd752e1d
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-11-29 12:26:15 +0100

    deveres

--- Tema_07/Parte 1/S07Ejercicio13.java
+++ Tema_07/Parte 1/S07Ejercicio13.java
@@ -4,50 +4,74 @@ public class S07Ejercicio13 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
-    String verde = "\033[32m";
-    String blanco = "\033[37m";
-    int[] n = new int[100];
-    int i;
-    int maximo = 0;
-    int minimo = 100;
+    Sint i;
+    String[] color = {
+      "verde", "rojo", "azul", "amarillo", "naranja", "rosa", "negro ", "blanco", "morado"
+    };
+
+    String[] palabra = new String[8];
+    String[] resultado = new String[8];
+
+    System.out.println("Introduzca 8 palabras (separado por INTRO): ");
     
-    for(i = 0; i < 100; i++) {
-      n[i] = (int)(Math.random()*501);
-      if(n[i] < minimo) {
-        minimo = n[i];
-      } 
-      if(n[i] > maximo) {
-        maximo = n[i];
-      }
-    }
+    int j = 0;
     
-    for(i = 0; i < 100; i++) {
-      System.out.print(n[i] + " ");
-    }
-    System.out.println();
-    
-    System.out.print("\n¿Qué quiere destacar? (1- mínimo, 2- máximo): ");
-    int numero = s.nextInt();
-    
-    // destacar
-    int destacar;
-    
-    if(numero == 1) {
-      destacar = minimo;
-    } else {
-      destacar = maximo;
-    }
-    
-    // resultado
-    for(i = 0; i < 100; i++) {
-      if(n[i] == destacar) {
-        System.out.print(verde + " **" + n[i] + "** " + blanco);
-      } else {
-        System.out.print(blanco + n[i] + " ");
-      }
-    }
-    System.out.println();
+    for (i = 0; i < 8; i++) {
+      palabra[i] = s.next();
       
+      // Si la palabra introducida es un color.
+      for (String c : color) {
+        if (palabra[i].equals(c)) {
+          resultado[j++] = c;
+        }
+      }
+    }
+    
+    // La palabra no es color
+    for (i = 0; i < 8; i++) {
+      boolean esColor = false;
+
+      for (String c : color) {
+        if (palabra[i].equals(c)) {
+          esColor = true;
+        }
+      }
+
+      if (!esColor) {
+        resultado[j++] = palabra[i];
+      }
+    }
+
+    // Array original.
+    System.out.println("\n\nArray original:");
+    for (i = 0; i < 8; i++) {
+      System.out.printf("│%7d ", i);
+    }
+    System.out.println("|");
+    for(i = 0; i < 75; i++) {
+      System.out.print("-");
+    }
+    System.out.println("-");
+    for (String p : palabra) {
+      System.out.printf("│%-7s ", p);
+    }
+    System.out.println("|");
+
+    // Array resultado.
+    System.out.println("\n\nArray original:");
+    for (i = 0; i < 8; i++) {
+      System.out.printf("│%7d ", i);
+    }
+    System.out.println("|");
+    for(i = 0; i < 75; i++) {
+      System.out.print("-");
+    }
+    System.out.println("-");
+    for (String r: resultado) {
+      System.out.printf("│%-7s ", r);
+    }
+    System.out.println("|");
+    
   }
 }
 
