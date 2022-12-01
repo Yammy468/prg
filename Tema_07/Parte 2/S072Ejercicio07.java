e3a21a413b8c6f625e7d11ad601d24621e88d6bf
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-12-01 12:19:50 +0100

    busca tesoro

--- Tema_07/Parte 2/S072Ejercicio07.java
+++ Tema_07/Parte 2/S072Ejercicio07.java
@@ -1,74 +1,62 @@
 import java.util.Scanner;
 
 public class S072Ejercicio07 {
-  // Se definen constantes para representar el contenido de las celdas
+
   static final int VACIO = 0;
   static final int MINA = 1;
   static final int TESORO = 2;
   static final int INTENTO = 3;
   
-  public static void main(String[] args) {
-
-    int[][] cuadrante = new int[5][4];
-
+  public static void main(String agrs[]) {
+    Scanner s = new Scanner(System.in);
+    
+    int[][] cuadrante = new int[4][5];
     int x;
     int y;
     
-    // Inicializa el array
+    // inicio del array
     for(x = 0; x < 4; x++) {
       for(y = 0; y < 3; y++) {
         cuadrante[x][y] = VACIO;
       }
     }
-    
-    // Coloca la mina
+      
+    // Colocacion de la mina
     int minaX = (int)(Math.random()*4);
     int minaY = (int)(Math.random()*3);
-    
     cuadrante[minaX][minaY] = MINA;
-    // Para depuración:
-    // System.out.println(minaX + ", " + minaY);
     
-    // Coloca el tesoro
-    int tesoroX;
-    int tesoroY;
-    
+    // Colocacion del tesoro
+    int tesoroX = (int)(Math.random()*4);
+    int tesoroY = (int)(Math.random()*3);
     do {
-      tesoroX = (int)(Math.random()*4);
-      tesoroY = (int)(Math.random()*3);
-    } while ((minaX == tesoroX) && (minaY == tesoroY));
-    
+    } while((minaX == tesoroX) && (minaY == tesoroY));
     cuadrante[tesoroX][tesoroY] = TESORO;
     
-    // Juego
-    System.out.println("¡BUSCA EL TESORO!");
-    
-    int oportunidades = 6;
+    System.out.println(" !BUSCA TESORO! ");
     boolean salir = false;
     String c = "";
-            
     do {
-      // Pinta el cuadrante
+      // pinta el cuadrante
       for(y = 3; y >= 0; y--) {
-        System.out.print(y + "|");  
+        System.out.print(y + "|");
         for(x = 0; x < 5; x++) {
-          if (cuadrante[x][y] == INTENTO) {
+          if (cuadrante[x][y] == INTENTO)
             System.out.print("X ");
-          } else { 
-            System.out.print("  ");
-          }
+          else
+            System.out.print(" ");
         }
-        System.out.println();   
+        System.out.println();
       }
-      System.out.println("  ----------\n  0 1 2 3 4\n");
+      System.out.println(" ----------\n 0 1 2 3 4\n");
       
-      // Pide las coordenadas
+      // pide las coordenadas
       System.out.print("Coordenada x: ");
-      x = Integer.parseInt(System.console().readLine());
+      x = s.nextInt();
       System.out.print("Coordenada y: ");
-      y = Integer.parseInt(System.console().readLine());
+      y = s.nextInt();
       
-      // Mira lo que hay en las coordenadas indicadas por el usuario
+      // mira lo que hay en las coordenadas indicadas por el usuario
       switch(cuadrante[x][y]) {
         case VACIO:
           cuadrante[x][y] = INTENTO;
@@ -80,19 +68,20 @@ public class S072Ejercicio07 {
           System.out.println("Lo siento, has perdido.");
           salir = true;
           break;
-        case TESORO:
-          System.out.println("Enhorabuena, has encontrado el tesoro."); 
-          salir = true;  
+        case TESORO:System.out.println("Enhorabuena, has encontrado el tesoro.");
+          salir = true;
+          break;
+        default:
       }
     } while (!salir);
-
-    // Pinta el cuadrante
+    
+    // pinta el cuadrante
     for(y = 3; y >= 0; y--) {
       System.out.print(y + " ");
       for(x = 0; x < 5; x++) {
         switch(cuadrante[x][y]) {
           case VACIO:
-            c = "  ";
+            c = " ";
             break;
           case MINA:
             c = "* ";
@@ -103,12 +92,13 @@ public class S072Ejercicio07 {
           case INTENTO:
             c = "x ";
             break;
+          default:
         }
         System.out.print(c);
-      }      
+      }
       System.out.println();
     }
-    System.out.println("  ----------\n  0 1 2 3 4\n");
+    System.out.println(" ----------\n 0 1 2 3 4\n");
     
    }
   }
