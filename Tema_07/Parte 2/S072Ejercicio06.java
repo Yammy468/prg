5587426d76f2fe872b336076c8fa42846711a920
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-12-01 11:46:18 +0100

    finalizado

--- Tema_07/Parte 2/S072Ejercicio06.java
+++ Tema_07/Parte 2/S072Ejercicio06.java
@@ -2,83 +2,68 @@ import java.util.Scanner;
 
 public class S072Ejercicio06 {
   public static void main(String[] args) 
-  throws InterruptedException {
- 
-    int[][] num = new int[6][10];
-
-    int fila;
-    int columna;
-
-    // Genera el contenido del array sin que se repita ningún valor
-    boolean repetido;
+  Scanner s = new Scanner(System.in);
+    
+    int[][] n = new int[6][10];
+    int fila, columna;
+    
+    int maximo = 0;
+    int maximoFila = 0;
+    int maximoColumna = 0;
+    
+    int minimo = 1000;
+    int minimoFila = 0;
+    int minimoColumna = 0;
+    
+    boolean repetido = true;
     int i;
-    int j;
-      
+    
+    // datos
     for(fila = 0; fila < 6; fila++) {
       for(columna = 0; columna < 10; columna++) {
+        // comprobar que no se repite
         do {
-          num[fila][columna] = (int)(Math.random() * 1001);
-
-          // Comprueba si el número generado ya está en el array.
-          repetido = false;
+        n[fila][columna] = (int)(Math.random()*1001);
+        repetido = false;
           for (i = 0; i < 10 * fila + columna; i++) {
-            if (num[fila][columna] == num[i / 10][i % 10]) {
+            if (n[fila][columna] == n[i / 10][i % 10]) {
               repetido = true;
             }
           }
         } while (repetido);
+        
+        // minimo y maximo
+        if(n[fila][columna] < minimo) {
+          minimo = n[fila][columna];
+          minimoFila = fila;
+          minimoColumna = columna;
+        }
+        if(n[fila][columna] > maximo) {
+          maximo = n[fila][columna];
+          maximoFila = fila;
+          maximoColumna = columna;
+        }
       }
     }
-
-    int minimo = Integer.MAX_VALUE;
-    int filaMinimo = 0;
-    int columnaMinimo = 0;
     
-    int maximo = Integer.MIN_VALUE;
-    int filaMaximo = 0;
-    int columnaMaximo = 0;
-
-    System.out.print("\n      ");
+    // tabla
+    System.out.print("        ");
     for(columna = 0; columna < 10; columna++) {
-      System.out.print("   " + columna + "  ");
+      System.out.print("  Columna " + columna);
+    }
+    
+    for(fila = 0; fila < 6; fila++) {
+      System.out.print("\nFila " + fila);
+      for(columna = 0; columna < 10; columna++) {
+        System.out.printf("%9d  ", n[fila][columna]);
+      }
     }
     System.out.println();
     
-    System.out.print("    ┌");
-    for(columna = 0; columna < 10; columna++) {
-      System.out.print("──────");
-    }
-    System.out.println("┐");
+    System.out.println("\nNúmero MÁXIMO: " + maximo);
+    System.out.println("Fila " + maximoFila + ", columna " + maximoColumna);
+    System.out.println("\nNúmero MÍNIMO: " + minimo);
+    System.out.println("Fila " + minimoFila + ", columna " + minimoColumna);
     
-    for(fila = 0; fila < 6; fila++) {
-      System.out.print("  " + fila + " │");
-      for(columna = 0; columna < 10; columna++) {
-        System.out.printf("%5d ", num[fila][columna]);
-        Thread.sleep(100);
-        
-        // Calcula el mínimo y guarda sus coordenadas
-        if (num[fila][columna] < minimo) {
-          minimo = num[fila][columna];
-          filaMinimo = fila;
-          columnaMinimo = columna;
-        }
-        
-        // Calcula el máximo y guarda sus coordenadas
-        if (num[fila][columna] > maximo) {
-          maximo = num[fila][columna];
-          filaMaximo = fila;
-          columnaMaximo = columna;
-        }
-      }
-      System.out.println("│");
-    }
-    System.out.print("    └");
-    for(columna = 0; columna < 10; columna++) {
-      System.out.print("──────");
-    }
-
-    System.out.println("┘\n\nEl máximo es " + maximo + " y está en la fila " + filaMaximo + ", columna " + columnaMaximo);
-    System.out.println("El mínimo es " + minimo + " y está en la fila " + filaMinimo + ", columna " + columnaMinimo);
-  
    }
   }
