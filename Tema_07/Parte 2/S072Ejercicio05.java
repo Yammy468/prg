a6b0024de3699638eccf6726aadf169f60c5557a
Yammy <113978794+Yammy468@users.noreply.github.com>
2022-12-01 10:38:43 +0100

    hecho final

--- Tema_07/Parte 2/S072Ejercicio05.java
+++ Tema_07/Parte 2/S072Ejercicio05.java
@@ -1,64 +1,57 @@
 import java.util.Scanner;
 
 public class S072Ejercicio05 {
-  public static void main(String[] args) 
-  throws InterruptedException {
- 
-    int[][] num = new int[6][10];
-
-    int fila;
-    int columna;
- 
-    int minimo = Integer.MAX_VALUE;
-    int filaMinimo = 0;
-    int columnaMinimo = 0;
+  public static void main(String[] args) {
+  Scanner s = new Scanner(System.in);
     
-    int maximo = Integer.MIN_VALUE;
-    int filaMaximo = 0;
-    int columnaMaximo = 0;
-
-    System.out.print("\n      ");
+    int[][] n = new int[6][10];
+    int fila, columna;
+    
+    int maximo = 0;
+    int maximoFila = 0;
+    int maximoColumna = 0;
+    
+    int minimo = 1000;
+    int minimoFila = 0;
+    int minimoColumna = 0;
+    
+    // datos
+    for(fila = 0; fila < 6; fila++) {
+      for(columna = 0; columna < 10; columna++) {
+        n[fila][columna] = (int)(Math.random()*1001);
+        
+        // minimo y maximo
+        if(n[fila][columna] < minimo) {
+          minimo = n[fila][columna];
+          minimoFila = fila;
+          minimoColumna = columna;
+        }
+        if(n[fila][columna] > maximo) {
+          maximo = n[fila][columna];
+          maximoFila = fila;
+          maximoColumna = columna;
+        }
+      }
+    }
+    
+    // tabla
+    System.out.print("        ");
     for(columna = 0; columna < 10; columna++) {
-      System.out.print("   " + columna + "  ");
+      System.out.print("  Columna " + columna);
+    }
+    
+    for(fila = 0; fila < 6; fila++) {
+      System.out.print("\nFila " + fila);
+      for(columna = 0; columna < 10; columna++) {
+        System.out.printf("%9d  ", n[fila][columna]);
+      }
     }
     System.out.println();
     
-    System.out.print("    ┌");
-    for(columna = 0; columna < 10; columna++) {
-      System.out.print("──────");
-    }
-    System.out.println("┐");
+    System.out.println("\nNúmero MÁXIMO: " + maximo);
+    System.out.println("Fila " + maximoFila + ", columna " + maximoColumna);
+    System.out.println("\nNúmero MÍNIMO: " + minimo);
+    System.out.println("Fila " + minimoFila + ", columna " + minimoColumna);
     
-    for(fila = 0; fila < 6; fila++) {
-      System.out.print("  " + fila + " │");
-      for(columna = 0; columna < 10; columna++) {
-        num[fila][columna] = (int)(Math.random() * 1001);
-        System.out.printf("%5d ", num[fila][columna]);
-        Thread.sleep(100);
-        
-        // Calcula el mínimo y guarda sus coordenadas
-        if (num[fila][columna] < minimo) {
-          minimo = num[fila][columna];
-          filaMinimo = fila;
-          columnaMinimo = columna;
-        }
-        
-        // Calcula el máximo y guarda sus coordenadas
-        if (num[fila][columna] > maximo) {
-          maximo = num[fila][columna];
-          filaMaximo = fila;
-          columnaMaximo = columna;
-        }
-      }
-      System.out.println("│");
-    }
-    System.out.print("    └");
-    for(columna = 0; columna < 10; columna++) {
-      System.out.print("──────");
-    }
-
-    System.out.println("┘\n\nEl máximo es " + maximo + " y está en la fila " + filaMaximo + ", columna " + columnaMaximo);
-    System.out.println("El mínimo es " + minimo + " y está en la fila " + filaMinimo + ", columna " + columnaMinimo);
-  
    }
   }
